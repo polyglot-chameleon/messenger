b90e00c0625fb0812a4e592e8df812ec220283b1
polyglot-chameleon <61985465+polyglot-chameleon@users.noreply.github.com>
2024-08-21 20:55:45 +0200

    4 create data binding (#5)
    * add data binding for basic chat func #4
    
    * text area clearing upon submit, fix #4

--- chat/chat.go
+++ chat/chat.go
@@ -1,35 +1,35 @@
 package chat
 
 import (
-	"log"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
 var Chat *fyne.Container
 
-var data = []string{"a", "string", "list"}
-
 func init() {
+	data := binding.BindStringList(&[]string{})
+
 	textArea := widget.NewMultiLineEntry()
 
-	list := widget.NewList(
-		func() int {
-			return len(data)
-		},
+	list := widget.NewListWithData(data,
+
 		func() fyne.CanvasObject {
 			return widget.NewLabel("template")
 		},
-		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(data[i])
+		func(i binding.DataItem, o fyne.CanvasObject) {
+			o.(*widget.Label).Bind(i.(binding.String))
 		})
 
 	form := &widget.Form{
 		Items: []*widget.FormItem{
 			{Widget: textArea}},
-		OnSubmit: func() { log.Println("Form submitted:", textArea.Text) },
+		OnSubmit: func() {
+			data.Append(textArea.Text)
+			textArea.SetText("")
+		},
 	}
 
 	Chat = container.NewBorder(nil, form, nil, nil, list)
